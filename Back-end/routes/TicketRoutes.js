a2d7f89b559def78cd88351ca6f62d373bb6781f
Thamilzhselvan5 <tamilselvank2823.com>
2025-07-24 11:26:51 +0530

    TicketController modified

--- Back-end/routes/TicketRoutes.js
+++ Back-end/routes/TicketRoutes.js
@@ -1,7 +1,7 @@
 // routes/ticketRoutes.js
 const express = require('express');
 const router = express.Router();
-const ticketController = require('../controllers/ticketController'); // <--- **THIS IS THE CRITICAL FIX!**
+const ticketController = require('../controllers/TicketController'); // <--- **THIS IS THE CRITICAL FIX!**
 
 router.post('/', ticketController.submitTicket); // Changed to submitTicket based on your controller exports
 router.get('/', ticketController.getAllTickets);
